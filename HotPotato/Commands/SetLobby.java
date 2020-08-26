5f195303979df9275ed034dbe262aa5f6f2f165b
Clovercola <40920988+Clovercola@users.noreply.github.com>
2020-08-26 00:37:13 -0400

    Fixed missing return.
    [BUGFIX] Fixed missing return in SetLobby class that sent a failure and a success message together.

--- HotPotato/Commands/SetLobby.java
+++ HotPotato/Commands/SetLobby.java
@@ -9,7 +9,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-import me.CloverCola.HotPotato.LocationManager;
+import me.CloverCola.HotPotato.ConfigUtilities.LocationManager;
 import me.CloverCola.HotPotato.StorageUtilities.LocationSerializationUtility;
 
 public class SetLobby {
