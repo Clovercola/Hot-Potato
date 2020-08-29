dc06ebcda229915f72b7981c01ba05d0ffdb1d68
Clovercola <40920988+Clovercola@users.noreply.github.com>
2020-08-29 14:54:26 -0400

    Fixed NPE
    [Bugfix] Initialized array for waitingPlayers because it was throwing a NPE without that. You can really tell this is the first time I've tested the plugin in a few days.

--- HotPotato/DataClasses/ArenaStatus.java
+++ HotPotato/DataClasses/ArenaStatus.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 
 public class ArenaStatus {
 	
-	private ArrayList<Player> waitingPlayers;
+	private ArrayList<Player> waitingPlayers = new ArrayList<Player>();
 	private boolean started;
 	
 	public ArenaStatus() {
