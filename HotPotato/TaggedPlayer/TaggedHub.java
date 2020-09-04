a2af509670da9a2972be3110e48d8c3b0c12dc6b
Clovercola <40920988+Clovercola@users.noreply.github.com>
2020-09-03 22:02:54 -0400

    Added disarm() method to TaggedHub

--- HotPotato/TaggedPlayer/TaggedHub.java
+++ HotPotato/TaggedPlayer/TaggedHub.java
@@ -1,6 +1,8 @@
 package me.CloverCola.HotPotato.TaggedPlayer;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import me.CloverCola.HotPotato.StatusCheck;
 
@@ -20,6 +22,14 @@ public class TaggedHub {
 		return;
 	}
 	
-	
+	//Aside from being a cutesy name, this helps communicate what this method does.
+	//I swear that's genuinely why I chose it.
+	public static void disarm(Player player) {
+		StatusCheck.setTagged(player, false);
+		ItemStack air = new ItemStack(Material.AIR);
+		player.getInventory().setHelmet(air);
+		player.getInventory().setItemInOffHand(air);
+		return;
+	}
 
 }
