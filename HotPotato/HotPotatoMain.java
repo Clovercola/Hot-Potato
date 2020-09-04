67d7f7dc78ed344c9f4360fea29322d42ee06e8a
Clovercola <40920988+Clovercola@users.noreply.github.com>
2020-09-04 16:40:52 -0400

    Changed how static is passed in HotPotatoMain

--- HotPotato/HotPotatoMain.java
+++ HotPotato/HotPotatoMain.java
@@ -6,32 +6,41 @@ import me.CloverCola.HotPotato.ConfigUtilities.LocationManager;
 import me.CloverCola.HotPotato.SafetyEvents.PlayerQuit;
 import me.CloverCola.HotPotato.SafetyEvents.PreventFireworkDamage;
 import me.CloverCola.HotPotato.SafetyEvents.PreventItemMovement;
+import me.CloverCola.HotPotato.TaggedPlayer.PotatoPass;
+import me.CloverCola.HotPotato.TaggedPlayer.PotatoTimer;
 import me.CloverCola.HotPotato.TaggedPlayer.TaggedFireworks;
 
 public class HotPotatoMain extends JavaPlugin{
 	
-	private final HotPotatoMain instance = this;
+	public static HotPotatoMain plugin;
 
 	@Override
 	public void onEnable() {
+		plugin = this;
 		getCommand("hotPotato").setExecutor(new Commands());
 		this.getServer().getPluginManager().registerEvents(new PreventItemMovement(), this);
-		this.getServer().getPluginManager().registerEvents(new PotatoTimer(instance), this);
+		this.getServer().getPluginManager().registerEvents(new PotatoTimer(plugin), this);
 		this.getServer().getPluginManager().registerEvents(new PlayerQuit(), this);
-		this.getServer().getPluginManager().registerEvents(new TaggedFireworks(instance), this);
+		this.getServer().getPluginManager().registerEvents(new TaggedFireworks(plugin), this);
 		this.getServer().getPluginManager().registerEvents(new PreventFireworkDamage(), this);
+		this.getServer().getPluginManager().registerEvents(new PotatoPass(), this);
 		this.saveConfig();
-		LocationManager initialize = new LocationManager(instance);
+		LocationManager initialize = new LocationManager(plugin);
 		initialize.saveConfig();
 		PlayerInventoryStorage psInit = new PlayerInventoryStorage();
-		psInit.setPluginInstance(instance);
-		StatusCheck.getPluginInstance(instance);
-		StartArena.setPluginInstance(instance);
+		psInit.setPluginInstance(plugin);
+		StatusCheck.getPluginInstance(plugin);
+		StartArena.setPluginInstance(plugin);
 		}
 	
 	@Override
 	public void onDisable() {
 		StatusCheck.emptyAllArenas();
 	}
+	
+	public static HotPotatoMain getInstance() {
+		return plugin;
+	}
+	
 	
 }
