262199ba8126c2175bad91dde5c14b5f8af73807
Clovercola <40920988+Clovercola@users.noreply.github.com>
2020-09-19 12:04:17 -0400

    Created new class "MetaHandler" to handle metadata
    This new class will handle most metadata (read: metadata not related to a specific class) to cut down on unnecessary methods in StatusCheck and make it easier to find methods by having them under an appropriate label. It will also help make it easier to debug StatusCheck since I won't have to be making sure it's not a Metadata problem happening in StatusCheck.

---
+++ HotPotato/MetaHandler.java
@@ -0,0 +1,83 @@
+package me.CloverCola.HotPotato;
+
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+
+import me.CloverCola.HotPotato.DataClasses.PlayerArenaStatus;
+
+//Handles all misc. metadata functions not related to a specific class.
+public class MetaHandler {
+
+	public static void setJoinedMeta(Player player, String arenaName) {
+		PlayerArenaStatus status = new PlayerArenaStatus(player, arenaName);
+		FixedMetadataValue data = new FixedMetadataValue(HotPotatoMain.getPlugin(), status);
+		player.setMetadata("HotPotatoStatus", data);
+		return;
+	}
+
+	public static boolean inArena(Player player) {
+		if (player.hasMetadata("HotPotatoStatus") == false) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean isAlive(Player player) {
+		if (inArena(player) == false) {
+			return false;
+		}
+		PlayerArenaStatus status = getStatusMetadata(player);
+		return status.isPlayerAlive();
+	}
+
+	public static boolean isTagged(Player player) {
+		if (isAlive(player) == false) {
+			return false;
+		}
+		PlayerArenaStatus status = getStatusMetadata(player);
+		if (status.isTagged() == false) {
+			return false;
+		}
+		return true;
+	}
+
+	// This method assumes you have checked if the player is in an arena first.
+	public static void setTagged(Player player, boolean tagged, String arenaName) {
+		PlayerArenaStatus status = getStatusMetadata(player);
+		status.setTagged(tagged);
+		if (tagged == true) {
+			StatusCheck.setTaggedInArena(arenaName, player);
+		}
+		return;
+	}
+
+	private static PlayerArenaStatus getStatusMetadata(Player player) {
+		MetadataValue data = player.getMetadata("HotPotatoStatus").get(0);
+		if (checkForMetaError(data) == false) {
+			emergencyError(player);
+		}
+		PlayerArenaStatus status = (PlayerArenaStatus) data.value();
+		return status;
+	}
+
+	private static void emergencyError(Player player) {
+		Bukkit.getLogger().log(Level.SEVERE, "Error grabbing Metadata for player " + player.getDisplayName() + "!");
+		Bukkit.getLogger().log(Level.INFO,
+				"Kicking " + player.getDisplayName() + " from the HotPotato game to prevent errors!");
+		player.sendMessage(ChatColor.DARK_RED + "A serious error occured, and you have been kicked from the game!");
+		StatusCheck.leave(player);
+	}
+
+	private static boolean checkForMetaError(MetadataValue data) {
+		if (!(data.value() instanceof PlayerArenaStatus)) {
+			return false;
+		}
+		return true;
+	}
+
+}
