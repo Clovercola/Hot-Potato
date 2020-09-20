90423499f8d8743dee1b1072d3e28def50ea9176
Clovercola <40920988+Clovercola@users.noreply.github.com>
2020-09-20 05:56:15 -0400

    Changed syntax

--- HotPotato/TaggedPlayer/TaggedHub.java
+++ HotPotato/TaggedPlayer/TaggedHub.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import me.CloverCola.HotPotato.MetaHandler;
+import me.CloverCola.HotPotato.StatusCheck;
 
 /**
  * A simple class meant to connect other "TaggedPlayer" classes together. Use
@@ -16,7 +17,8 @@ import me.CloverCola.HotPotato.MetaHandler;
 public class TaggedHub {
 
 	public static void activate(Player player, String arenaName) {
-		MetaHandler.setTagged(player, true, arenaName);
+		MetaHandler.setTagged(player, true);
+		StatusCheck.setTaggedInArena(arenaName, player);
 		TaggedPlayerItems.equipItems(player);
 		TaggedFireworks.fireworksTimer(player);
 		return;
@@ -25,7 +27,7 @@ public class TaggedHub {
 	//Aside from being a cutesy name, this helps communicate what this method does.
 	//I swear that's genuinely why I chose it.
 	public static void disarm(Player player, String arenaName) {
-		MetaHandler.setTagged(player, false, arenaName);
+		MetaHandler.setTagged(player, false);
 		ItemStack air = new ItemStack(Material.AIR);
 		player.getInventory().setHelmet(air);
 		player.getInventory().setItemInOffHand(air);
