273283009c1830bb44b849595ac81977bd9f1bc4
Clovercola <40920988+Clovercola@users.noreply.github.com>
2020-09-23 17:23:52 -0400

    Updated how PlayerEliminated Works + More
    PlayerEliminated now functions differently than if the player left the game, and will no longer ask to choose a new tagged player if the "Win Condition" is true.

--- HotPotato/TaggedPlayer/PlayerEliminated.java
+++ HotPotato/TaggedPlayer/PlayerEliminated.java
@@ -3,24 +3,26 @@ package me.CloverCola.HotPotato.TaggedPlayer;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
-import me.CloverCola.HotPotato.MetaHandler;
 import me.CloverCola.HotPotato.StatusCheck;
+import me.CloverCola.HotPotato.WinCondition;
 
 public class PlayerEliminated {
-
-	// This class will change a LOT.
-	//It's pretty barebones right now because I'm cleaning up my code next.
-	//So this will get a method or two in order to get the tagged player directly.
+	
 	public static void eliminate(String arenaName) {
-		Player player;
-		for (int i = 0; i < StatusCheck.getPlayerCount(arenaName); i++) {
-			player = StatusCheck.getPlayerFromArena(arenaName, i);
-			if (MetaHandler.isTagged(player) == true) {
-				player.sendMessage(ChatColor.RED + "You were eliminated!");
-				StatusCheck.leave(player);
-				break;
-			}
+		Player player = StatusCheck.getTaggedFromArena(arenaName);
+		player.sendMessage(ChatColor.RED + "You were eliminated! Better luck next time!");
+		StatusCheck.removePlayer(player);
+		StatusCheck.setTaggedInArena(null);
+		chooseNewTagged(arenaName);
+		return;
+	}
+	
+	private static void chooseNewTagged(String arenaName) {
+		if (WinCondition.check(arenaName) == true) {
+			return;
 		}
+		ChooseTaggedPlayer.randomTaggedPlayer(arenaName);
+		return;
 	}
 
 }
